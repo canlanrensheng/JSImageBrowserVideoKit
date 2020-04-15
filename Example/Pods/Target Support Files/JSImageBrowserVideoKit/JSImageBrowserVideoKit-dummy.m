7290944035c32534faa96b6908966f9ad3372ddb
张金山 <jinshan.zhang@51ysscw.com>
2020-04-15 11:28:30 +0800

    1.0.0代码提交

---
+++ Example/Pods/Target Support Files/JSImageBrowserVideoKit/JSImageBrowserVideoKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JSImageBrowserVideoKit : NSObject
+@end
+@implementation PodsDummy_JSImageBrowserVideoKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-JSImageBrowserVideoKit_Tests/Pods-JSImageBrowserVideoKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JSImageBrowserVideoKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JSImageBrowserVideoKit_Tests
+@end
